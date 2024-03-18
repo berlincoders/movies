4a61ac4018a3dd3a5e882f23afc30ec42dd0924b
Romeo <ralonsoad@gmail.com>
2024-03-18 22:31:43 +0100

    Movies, API visible.

---
+++ src/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <div className="menu">
+      {/* Your menu items */}
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Menu;
