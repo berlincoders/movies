c0c9a100bedd3b596d5384b56d90f73548812fc5
Romeo <ralonsoad@gmail.com>
2024-03-21 00:58:31 +0100

    Adding Boostrap & Navbar

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -2,14 +2,27 @@ import React from 'react';
 
 const Menu = () => {
   return (
-    <div className="menu">
-      {/* Your menu items */}
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Navbar</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
